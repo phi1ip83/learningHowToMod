734bb373e70c5697e4ffe3e2b7350400b226b4c5
phi1ip83 <philip.dossantos@hotmail.com>
2022-06-19 14:42:48 -0400

    Initial mod setup.

---
+++ src/main/java/io/github/phi1ip83/learningmod/LearningMod.java
@@ -0,0 +1,8 @@
+package io.github.phi1ip83.learningmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LearningMod.MOD_ID)
+public class LearningMod {
+    public static final String MOD_ID = "learningmod";
+}
